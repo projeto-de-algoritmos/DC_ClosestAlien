1052f8b661fe300222e3897cb4e1aa871194bb3b
CorreiaJV <correia.jv16@gmail.com>
2022-12-24 15:15:48 -0300

    Adição dinâmica com alien e pontuação

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 
 class Player(pygame.sprite.Sprite):
 
-    def __init__(self, gs=None):
+    def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.images = [pygame.image.load("images/spaceU.png"), pygame.image.load("images/spaceD.png") , pygame.image.load("images/spaceR.png"), pygame.image.load("images/spaceL.png")]
         self.current_image = pygame.transform.scale(self.images[0], (50, 50))
@@ -41,15 +41,31 @@ pygame.display.set_caption("Navezinha")
 bg = pygame.image.load('images/bg2.jpg').convert_alpha()
 bg = pygame.transform.scale(bg, (x, y))
 
+alien = pygame.image.load('images/alien.png').convert_alpha()
+alien = pygame.transform.scale(alien, (50, 50))
 
 pos_player_x = 200
 pos_player_y = 300
 
+pos_alien_x = random.randint(1, 1200)
+pos_alien_y = random.randint(1, 640)
+
 play = True
 player = Player()
 
-player_rect = player.current_image.get_rect()
+points=0
 
+player_rect = player.current_image.get_rect()
+alien_rect = alien.get_rect()
+
+def colisions():
+    global points
+    if player_rect.colliderect(alien_rect):
+        points +=1
+        return True
+    else:
+        return False
+print(pygame.font.get_fonts())
 
 while play:
     for event in pygame.event.get():
@@ -62,7 +78,6 @@ while play:
     screen.blit(bg, (rel_x - bg.get_rect().width, 0))  # background
     if rel_x < 1280:
         screen.blit(bg, (rel_x, 0))
-
     
 
     key = pygame.key.get_pressed()
@@ -75,11 +90,11 @@ while play:
         pos_player_y += 1
         player.swap(direction=1)
 
-    if key[pygame.K_RIGHT] and pos_player_y < 665:
+    if key[pygame.K_RIGHT] and pos_player_x < 1225:
         pos_player_x += 1
         player.swap(direction=2)
 
-    if key[pygame.K_LEFT] and pos_player_y < 665:
+    if key[pygame.K_LEFT] and pos_player_x > 1:
         pos_player_x -= 1
         player.swap(direction=3)
 
@@ -88,10 +103,23 @@ while play:
     player_rect.y= pos_player_y
     player_rect.x= pos_player_x
 
-    x -= 0.5
+    alien_rect.y= pos_alien_y
+    alien_rect.x= pos_alien_x
+
+    if colisions():
+        pos_alien_x = random.randint(1, 1200)
+        pos_alien_y = random.randint(1, 640)
+
+    x -= 0.5 #Background Movement
+    
+    font = pygame.font.Font('freesansbold.ttf', 50)
+    score = font.render(f'Score: {int(points)}',True,(0,0,0))
+    screen.blit(score,(50,50))
 
     pygame.draw.rect(screen,(255,0,0), player_rect,4)
- 
+    pygame.draw.rect(screen,(255,0,0), alien_rect,4)
+
     screen.blit(player.current_image, (pos_player_x, pos_player_y))
+    screen.blit(alien, (pos_alien_x, pos_alien_y))
 
     pygame.display.update()
