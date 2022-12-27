4ef4c2b1dfce932dd3a86deccb1797fdcb461dd8
CorreiaJV <correia.jv16@gmail.com>
2022-12-27 12:24:51 -0300

    Adição algoritmo par de pontos mais próximos

--- algorithms.py
+++ algorithms.py
@@ -1,4 +1,6 @@
 import random
+import math
+import copy
 import sys
 sys.setrecursionlimit(100000)
 
@@ -48,3 +50,55 @@ vetor = [(random.random()*100*random.random()*100*random.random()*100) for i in 
 print('Mediana Real: ', sorted(vetor)[len(vetor)//2])
 mm = medianOfMedians(vetor, len(vetor)//2)
 print('Mediana Oráculo: ', mm)
+
+
+
+
+def dist(p1, p2):
+    return math.sqrt(((p2[1]-p1[1])**2)+((p2[0]-p1[0])**2))
+
+def closest_brute_force(points):
+    min_dist = float("inf")
+    p1 = None
+    p2 = None
+    for i in range(len(points)):
+        for j in range(i+1, len(points)):
+            d = dist(points[i], points[j])
+            if d < min_dist:
+                min_dist = d
+                p1 = points[i]
+                p2 = points[j]
+    return p1, p2, min_dist
+
+
+def rec(xsorted, ysorted):
+    n = len(xsorted)
+    if n <= 3:
+        return closest_brute_force(xsorted)
+    else:
+        midpoint = xsorted[n//2]
+        xsorted_left = xsorted[:n//2]
+        xsorted_right = xsorted[n//2:]
+        ysorted_left = []
+        ysorted_right = []
+        for point in ysorted:
+            ysorted_left.append(point) if (point[0] <= midpoint[0]) else ysorted_right.append(point)
+        (p1_left, p2_left, delta_left) = rec(xsorted_left, ysorted_left)
+        (p1_right, p2_right, delta_right) = rec(xsorted_right, ysorted_right)
+        (p1, p2, delta) = (p1_left, p2_left, delta_left) if (delta_left < delta_right) else (p1_right, p2_right, delta_right)
+        in_band = [point for point in ysorted if midpoint[0]-delta < point[0] < midpoint[0]+delta]
+        for i in range(len(in_band)):
+            for j in range(i+1, min(i+7, len(in_band))):
+                d = dist(in_band[i], in_band[j])
+                if d < delta:
+                    print(in_band[i], in_band[j])
+                    (p1, p2, delta) = (in_band[i], in_band[j], d)
+        return p1, p2, delta
+
+
+def closest(points):
+    xsorted = sorted(points, key=lambda point: point[0])
+    ysorted = sorted(points, key=lambda point: point[1])
+    return rec(xsorted, ysorted)
+  
+
